a58e7325b9857c3d07907d2f02a1e9885cb35bab
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-02-06 08:37:12 +0900

    찍어보는 리퀘스트 정보값 추가

--- src/main/java/com/javaex/controller/BoardController.java
+++ src/main/java/com/javaex/controller/BoardController.java
@@ -169,6 +169,11 @@ public class BoardController {
 	private void check(HttpServletRequest request) {
 		System.out.println("페이지를 요청한 ip 주소 (Host) : " + request.getRemoteHost());
 		System.out.println("페이지를 요청한 ip 주소 (Addr) : " + request.getRemoteAddr());
-		System.out.println("접근 포트 : " + request.getLocalPort());
+		System.out.println("LocalPort : " + request.getLocalPort());
+		System.out.println("LocalName : " + request.getLocalName());
+		System.out.println("Protocol : " + request.getProtocol());
+		System.out.println("Scheme : " + request.getScheme());
+		System.out.println("ContextPath : " + request.getContextPath());
+		System.out.println("URI : " + request.getRequestURI());
 	}
 }
--- src/main/java/com/javaex/controller/MainController.java
+++ src/main/java/com/javaex/controller/MainController.java
@@ -14,8 +14,12 @@ public class MainController {
 		
 		System.out.println("페이지를 요청한 ip 주소 (Host) : " + request.getRemoteHost());
 		System.out.println("페이지를 요청한 ip 주소 (Addr) : " + request.getRemoteAddr());
-		System.out.println("접근 포트 : " + request.getLocalPort());
-		
+		System.out.println("LocalPort : " + request.getLocalPort());
+		System.out.println("LocalName : " + request.getLocalName());
+		System.out.println("Protocol : " + request.getProtocol());
+		System.out.println("Scheme : " + request.getScheme());
+		System.out.println("ContextPath : " + request.getContextPath());
+		System.out.println("URI : " + request.getRequestURI());
 		
 		return "main/index";
 	}
